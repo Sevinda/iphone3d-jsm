b619e8a9fb5815b239d78c479dc938d5c6dffac4
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-06-24 15:12:18 +0530

    created the navbar and the hero section

---
+++ src/components/Highlights.jsx
@@ -0,0 +1,26 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import React from "react";
+
+const Highlights = () => {
+  useGSAP(() => {
+    gsap.to("#title", {opacity: 1, y: 0});
+  }, []);
+
+  return (
+    <section
+      id="highlights"
+      className="w-screen overflow-hidden h-full common-padding bg-zinc"
+    >
+      <div className="screen-max-width">
+        <div>
+          <h1 id="title" className="section-heading">
+            Get the highlights.
+          </h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Highlights;
